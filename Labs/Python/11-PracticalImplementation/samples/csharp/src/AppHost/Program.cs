7371083f17f691aae6d03a6d1105f72048b8ca94
ibeljan <beljan.ivan@gmail.com>
2025-10-30 15:49:36 +0100

    content implementation

--- Labs/Python/11-PracticalImplementation/samples/csharp/src/AppHost/Program.cs
+++
@@ -1,6 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.Calculator>("calc-mcp")
-	   .WithExternalHttpEndpoints();
-
-builder.Build().Run();
--- Labs/Python/11-PracticalImplementation/samples/java/containerapp/src/main/java/com/microsoft/cognitiveservices/Bot.java
+++
@@ -1,6 +0,0 @@
-package com.microsoft.cognitiveservices;
-
-public interface Bot {
-
-    String chat(String prompt);
-}
